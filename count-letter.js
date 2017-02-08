59b51f6d85c3fd75c62eb6db19407c888d5f05dd
hans kurniadi <hans.d.k@live.com>
2017-02-08 21:23:52 +0000

    refactor the codes, shorten variable names

--- count-letter.js
+++ count-letter.js
@@ -10,23 +10,21 @@ print all letters and their counts
 */
 
 var string = 'lighthouse in the house';
-var spaceRemovedString = string.split(' ').join('');
-//console.log(spaceRemovedString);
 
-function countLetter(string) {
+function countLetter(str) {
+  str = str.split(' ').join('');
   var storedLetters = {};
-  for (var i = 0; i < spaceRemovedString.length; i++) {
-    //console.log(spaceRemovedString[i]);
-    if (storedLetters.hasOwnProperty(spaceRemovedString[i])) {
-      //console.log(storedLetters.hasOwnProperty(spaceRemovedString[i]));
-      storedLetters[spaceRemovedString[i]] += 1;
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i]
+    if (storedLetters.hasOwnProperty(char)) {
+      storedLetters[char] += 1;
     } else {
-      storedLetters[spaceRemovedString[i]] = 1;
+      storedLetters[char] = 1;
     }
   }
   console.log(storedLetters);
 }
 
-countLetter(spaceRemovedString);
+countLetter(string);
 
 
